06b88fb77a23220579faad3f993d52864e57881c
Graeme Miller <graeme@solab.co.uk>
2015-12-03 01:49:25 +0000

    POCO Controller

---
+++ src/MVC6Awakens/Controllers/POCOController.cs
@@ -0,0 +1,29 @@
+﻿
+using Microsoft.AspNet.Http;
+using Microsoft.AspNet.Mvc;
+
+using Microsoft.AspNet.Mvc.Rendering;
+using Microsoft.AspNet.Mvc.ViewFeatures;
+
+
+namespace MVC6Awakens.Controllers
+{
+    public class POCOController
+    {
+        [ActionContext]
+        public ActionContext ActionContext { get; set; }
+
+
+
+
+        // GET: /<controller>/
+        public IActionResult Content()
+        {
+            return new ContentResult() { Content = "Hello from POCO controller!" };
+        }
+        public IActionResult Index()
+        {
+            return new ViewResult() { };
+        }
+    }
+}
