0fe201f813f97e7f6fd6eb55db5074b4333ae7a9
Graeme Miller <graeme@solab.co.uk>
2015-12-03 01:10:32 +0000

    Removed some unneeded namespaces

--- src/MVC6Awakens/ViewModels/Manage/RemoveLoginViewModel.cs
+++ src/MVC6Awakens/ViewModels/Manage/RemoveLoginViewModel.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace MVC6Awakens.ViewModels.Manage
+﻿namespace MVC6Awakens.ViewModels.Manage
 {
     public class RemoveLoginViewModel
     {
