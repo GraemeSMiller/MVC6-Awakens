db03fe9f5413abbeb87d3c2eda347e4afaecc41c
Graeme Miller <graeme@solab.co.uk>
2015-12-04 20:54:32 +0000

    API Controller for Weapon Of Choice

---
+++ src/MVC6Awakens/ApiControllers/WeaponOfChoiceController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+using Microsoft.AspNet.Mvc;
+
+using MVC6Awakens.Models;
+
+
+namespace MVC6Awakens.ApiControllers
+{
+    [Route("api/WeaponOfChoice")]
+    public class WeaponOfChoiceController
+    {
+        private readonly DomainContext context;
+
+        public WeaponOfChoiceController(DomainContext context)
+        {
+            this.context = context;
+        }
+
+        [HttpGet]
+        public IEnumerable<string> GetAll()
+        {
+            return context.Characters.Select(a => a.WeaponOfChoice).Distinct().OrderBy(a => a);
+        }
+    }
+}
