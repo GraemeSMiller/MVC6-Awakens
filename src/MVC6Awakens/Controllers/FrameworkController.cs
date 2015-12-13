d935b5ebf513063a5ceb6cddadacf49b71b33a6b
Graeme Miller <graeme@solab.co.uk>
2015-12-13 22:17:09 +0000

    Framework Controller example

---
+++ src/MVC6Awakens/Controllers/FrameworkController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNet.Mvc;
+using System.Reflection;
+
+namespace MVC6Awakens.Controllers
+{
+    public class FrameworkController : Controller
+    {
+        public string Index()
+        {
+            var assembly = "";
+            #if DNX451
+                // utilize resource only available with .NET Framework
+                assembly = Assembly.GetExecutingAssembly().FullName;
+            #endif
+            #if DNXCORE50
+                 // utilize resource only available with .NET Framework
+                assembly=typeof(FrameworkController).GetTypeInfo().Assembly.FullName;
+            #endif
+            return assembly;
+        }
+    }
+}
